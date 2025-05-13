8b84f715ac1c164f99327e872e0c749fff6e7d99
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-05-13 10:01:32 +0900

    Refactor: controller 추가 및 코드 분리

--- routes/email.js
+++ routes/email.js
@@ -1,25 +1,9 @@
 const express = require('express');
-const Users = require('../models/User');
+const emailController = require('../controllers/emailController');
 
 const router = express.Router();
 
 // 이메일 입력
-router.patch('/:id', async (req, res) => {
-    const { id } = req.params;
-    const { email } = req.body;
-    try {
-        const [isUpdated] = await Users.update({email}, {where: {id}});
-        if (isUpdated) {
-            res.send('이메일 수정 완료');
-        }
-        else {
-            res.status(404).send('이메일 찾기 실패');
-            return;
-        }
-    } catch (err) {
-        console.error('데이터베이스 쿼리 실패 : ', err);
-        res.status(500).send('Internal Server Error');
-    }
-});
+router.patch('/:id', emailController.addEmail);
 
 module.exports = router;
