f23becb4babea281293b141bf6563b27805b23da
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-05-29 19:21:19 +0900

    Feat: idol 테이블의 모든 값 넘겨주기

--- controllers/idolController.js
+++ controllers/idolController.js
@@ -4,7 +4,7 @@ const idolController = {
   // 아이돌 사진 반환
   getIdolAllImage: async (req, res) => {
     try {
-      const idolImg = await Idol.findAll({ attributes: ["idolImages"] });
+      const idolImg = await Idol.findAll({ attributes: ["id", "idolName", "idolImages", "videoCallCount"] });
       res.json(idolImg);
     } catch (err) {
       console.error(err);
