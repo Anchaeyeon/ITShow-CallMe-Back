2cb0229b4a3e3a6a716cde8d79b7306ed927f7c5
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-05-30 11:20:36 +0900

    아이돌 클릭 시 조회수 증가하도록

--- controllers/idolController.js
+++ controllers/idolController.js
@@ -1,6 +1,22 @@
 const { Idol } = require("../models");
 
 const idolController = {
+  clickIdol: async (req, res) => {
+    const id = req.params.id;
+    try {
+      const clickCount = await Idol.findByPk(id);
+      if (!clickCount) {
+        return res.status(404).send("아이돌 아이디가 존재하지 않습니다.");
+      }
+      clickCount.videoCallCount += 1;
+      await clickCount.save();
+      res.send("아이돌 클릭 수 증가");
+    } catch (err) {
+      console.error("클릭 수 증가 실패: ", err);
+      res.status(500).send("서버 에러");
+    }
+  },
+
   // 아이돌 사진 반환
   getIdolAllImage: async (req, res) => {
     try {
