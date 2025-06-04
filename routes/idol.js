92cb43d59dae94cd6a8da6a011fe5be109824a9e
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-06-04 09:23:25 +0900

    Feat: 선택한 아이돌 클릭 횟수 반환 구현

--- routes/idol.js
+++ routes/idol.js
@@ -9,4 +9,7 @@ router.get('/all', idolController.getIdolAllImage);
 // 아이돌 클릭 수 증가
 router.get('/click/:id', idolController.clickIdol);
 
+// 사용자가 선택한 아이돌 클릭 횟수 반환하기
+router.get('/click/oneIdol/:id', idolController.getclickOneIdol);
+
 module.exports = router;
