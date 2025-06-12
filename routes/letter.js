2f605ddc729b984dd00fe4e11de77676ee1e6200
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-06-13 02:43:38 +0900

    Feat: 선택한 아이돌의 편지만 보여주는 기능 구현

--- routes/letter.js
+++ routes/letter.js
@@ -9,8 +9,11 @@ router.get('/idolName/:id', letterController.getIdolName);
 // 편지 입력
 router.post('/message', letterController.addLetter);
 
+// 사용자가 고른 아이돌 한 명에 대한 편지 모두 반환
+router.get('/idolLetter/:id', letterController.getIdolLetter);
+
 // 메세지 1개 반환
-router.get('/message/:id', letterController.getMessageDetail);
+router.get('/message/:id', letterController.getOneMessageDetail);
 
 // 모든 메세지 반환
 router.get('/allLetter', letterController.getAllLetters);
