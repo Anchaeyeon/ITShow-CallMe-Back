ebe20c5ce4ac07e19ec5fd614ae173ed3d77dfaf
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-06-19 11:23:44 +0900

    feat: cors 최상단 위치

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const cors = require("cors");
+const express = require("express");
 const methodOverride = require("method-override");
 const path = require("path");
 
